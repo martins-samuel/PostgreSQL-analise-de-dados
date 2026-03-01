938010185b7ae31d416258639befdcec8ddd9f4d
Samuel Martins <samuelmartinsdealbuquerque@gmail.com>
2026-03-01 12:36:40 -0300

    Finalizando o curso

---
+++ 5-Unions/1Query.sql
@@ -0,0 +1,17 @@
+-- SINTAXE #########################################################################
+select coluna_1, coluna_2
+from schema_1.tabela_1
+
+union / union all
+
+select coluna_3, coluna_4 
+from schema_2.tabela_2
+
+
+-- EXEMPLOS ########################################################################
+
+-- (Exemplo 1) União simples de duas tabelas
+-- Una a tabela sales.products com a tabela temp_tables.products_2
+
+select * from sales.products
+UNION ALL
