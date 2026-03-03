bb63f06c06f31fe01ab5ceb0934f66d43f457b8b
Samuel Martins <samuelmartinsdealbuquerque@gmail.com>
2026-03-02 22:17:33 -0300

    01-03

---
+++ 7-TratamentoDados/2-Query.sql
@@ -0,0 +1,44 @@
+-- RESUMO ##########################################################################
+-- (1) O operador :: e o CAST() são funções utilizadas para converter um dado para 
+-- a unidade desejada. 
+-- (2) O operador :: é mais "clean", porém, em algumas ocasiões não funciona, sendo
+-- necessário utilizar a função CAST()
+-- (3) Use o Guia de comandos para consultar a lista de unidades mais utilizadas
+
+-- TIPOS ###########################################################################
+-- CASE WHEN
+-- COALESCE()
+
+
+-- EXEMPLOS ########################################################################
+
+-- (Exemplo 1) Agrupamento de dados com CASE WHEN
+-- Calcule o nº de clientes que ganham abaixo de 5k, entre 5k e 10k, entre 10k e 
+-- 15k e acima de 15k
+WITH faixa_renda AS (
+SELECT income,
+CASE 
+	WHEN income < 5000 then'5k'
+	WHEN income >= 5000 AND income < 10000 THEN '10K'
+	WHEN income >= 10000 AND income < 15000 THEN '15k'
+	ELSE '+15k'
+	END AS faixa_renda_grupo
+	FROM sales.customers
+)
+SELECT faixa_renda_grupo , COUNT(*)
+FROM faixa_renda
+GROUP BY faixa_renda_grupo;
+-- (Exemplo 2) Tratamento de dados nulos com COALESCE
+-- Crie uma coluna chamada populacao_ajustada na tabela temp_tables.regions e
+-- preencha com os dados da coluna population, mas caso esse campo estiver nulo, 
+-- preencha com a população média (geral) das cidades do Brasil
+
+
+-- RESUMO ##########################################################################
+-- (1) CASE WHEN é o comando utilizado para criar respostas específicas para 
+-- diferentes condições e é muito utilizado para fazer agrupamento de dados
+-- (2) COALESCE é o comando utilizado para preencher campos nulos com o primeiro
+-- valor não nulo de uma sequência de valores.
+
+
+
